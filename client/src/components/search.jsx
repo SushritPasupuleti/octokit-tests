294b52c686b1b87f1541a074c1eb3f5019c65b2f
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-03-25 17:56:49 +0530

    add feature to get repo details

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -7,53 +7,72 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import DirectionsIcon from '@material-ui/icons/Directions';
+const { Octokit } = require('@octokit/rest');
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: '2px 4px',
-    display: 'flex',
-    alignItems: 'center',
-    width: 300,
-  },
-  input: {
-    marginLeft: theme.spacing(1),
-    flex: 1,
-  },
-  iconButton: {
-    padding: 10,
-  },
-  divider: {
-    height: 28,
-    margin: 4,
-  },
+	root: {
+		padding: '2px 4px',
+		display: 'flex',
+		alignItems: 'center',
+		width: 300,
+	},
+	input: {
+		marginLeft: theme.spacing(1),
+		flex: 1,
+	},
+	iconButton: {
+		padding: 10,
+	},
+	divider: {
+		height: 28,
+		margin: 4,
+	},
 }));
 
 export default function Search() {
-  const classes = useStyles();
+	const classes = useStyles();
+	const octokit = new Octokit();
 
-  const [userName, setuserName] = useState("");
-  const [repoName, setrepoName] = useState("");
+	const [userName, setuserName] = useState('');
+	const [repoName, setrepoName] = useState('');
 
-  return (
-    <Paper component="form" className={classes.root}>
-      <InputBase
-        className={classes.input}
-        placeholder="User"
-        inputProps={{ 'aria-label': 'query' }}
-        value={userName}
-        onChange={e => setuserName(e.target.value)}
-      />
-      <Divider className={classes.divider} orientation="vertical" />
-      <InputBase
-        className={classes.input}
-        placeholder="Repo"
-        inputProps={{ 'aria-label': 'query2' }}
-        value={repoName}
-        onChange={e => setrepoName(e.target.value)}
-      />
-      <IconButton type="submit" className={classes.iconButton} aria-label="search">
-        <SearchIcon />
-      </IconButton>
-    </Paper>
-  );
+	const fetchRepoDetails = () => {
+		octokit.repos
+			.get({
+				owner: userName,
+				// type: 'public',
+        repo: repoName
+			})
+			.then(({ data }) => {
+				console.log(data);
+			});
+	};
+
+	return (
+		<Paper component="form" className={classes.root}>
+			<InputBase
+				className={classes.input}
+				placeholder="User"
+				inputProps={{ 'aria-label': 'query' }}
+				value={userName}
+				onChange={(e) => setuserName(e.target.value)}
+			/>
+			<Divider className={classes.divider} orientation="vertical" />
+			<InputBase
+				className={classes.input}
+				placeholder="Repo"
+				inputProps={{ 'aria-label': 'query2' }}
+				value={repoName}
+				onChange={(e) => setrepoName(e.target.value)}
+			/>
+			<IconButton
+				type="button"
+				className={classes.iconButton}
+				aria-label="search"
+				onClick={() => fetchRepoDetails()}
+			>
+				<SearchIcon />
+			</IconButton>
+		</Paper>
+	);
 }
